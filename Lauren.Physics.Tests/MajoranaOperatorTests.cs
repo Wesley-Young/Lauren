52bff1916e4ad1eb2a7646fc87e20111f9e6b4e2
Wei FeiYang <wesley.f.young@outlook.com>
2026-01-20 16:33:14 +0800

    [Physics] Add MajoranaOperator and tests

---
+++ Lauren.Physics.Tests/MajoranaOperatorTests.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using Lauren.Physics.Operators;
+using Xunit;
+
+namespace Lauren.Physics.Tests;
+
+public class MajoranaOperatorTests
+{
+    [Fact]
+    public void Multiply_WithOperator_ParityOdd_FlipsSign()
+    {
+        var leftX = new BitArray(1);
+        var leftZ = new BitArray(1) { [0] = true };
+        var left = new MajoranaOperator(leftX, leftZ, Coefficient.PlusI);
+
+        var rightX = new BitArray(1) { [0] = true };
+        var rightZ = new BitArray(1);
+        var right = new MajoranaOperator(rightX, rightZ, Coefficient.PlusOne);
+
+        var leftCopyX = new BitArray(left.OccupiedX);
+        var leftCopyZ = new BitArray(left.OccupiedZ);
+        var rightCopyX = new BitArray(right.OccupiedX);
+        var rightCopyZ = new BitArray(right.OccupiedZ);
+
+        var result = left.Multiply(right);
+
+        var op = Assert.IsType<MajoranaOperator>(result);
+        Assert.Equal(Coefficient.MinusI, op.Coefficient);
+        Assert.True(BitsEqual(new BitArray(1) { [0] = true }, op.OccupiedX));
+        Assert.True(BitsEqual(new BitArray(1) { [0] = true }, op.OccupiedZ));
+        Assert.True(BitsEqual(leftCopyX, left.OccupiedX));
+        Assert.True(BitsEqual(leftCopyZ, left.OccupiedZ));
+        Assert.True(BitsEqual(rightCopyX, right.OccupiedX));
+        Assert.True(BitsEqual(rightCopyZ, right.OccupiedZ));
+    }
+
+    [Fact]
+    public void Multiply_WithOperator_ParityEven_DoesNotFlipSign()
+    {
+        var leftX = new BitArray(2) { [0] = true };
+        var leftZ = new BitArray(2);
+        var left = new MajoranaOperator(leftX, leftZ, Coefficient.PlusI);
+
+        var rightX = new BitArray(2) { [1] = true };
+        var rightZ = new BitArray(2);
+        var right = new MajoranaOperator(rightX, rightZ, Coefficient.MinusOne);
+
+        var result = left.Multiply(right);
+
+        var op = Assert.IsType<MajoranaOperator>(result);
+        Assert.Equal(Coefficient.MinusI, op.Coefficient);
+        Assert.True(BitsEqual(new BitArray(2) { [0] = true, [1] = true }, op.OccupiedX));
+        Assert.True(BitsEqual(new BitArray(2), op.OccupiedZ));
+    }
+
+    [Fact]
+    public void Multiply_WithCoefficient_MultipliesCoefficientAndClonesBits()
+    {
+        var occupiedX = new BitArray(3) { [1] = true };
+        var occupiedZ = new BitArray(3) { [2] = true };
+        var op = new MajoranaOperator(occupiedX, occupiedZ, Coefficient.PlusI);
+
+        var result = op.Multiply(Coefficient.MinusOne);
+
+        var majorana = Assert.IsType<MajoranaOperator>(result);
+        Assert.Equal(Coefficient.MinusI, majorana.Coefficient);
+        Assert.True(BitsEqual(occupiedX, majorana.OccupiedX));
+        Assert.True(BitsEqual(occupiedZ, majorana.OccupiedZ));
+        Assert.False(ReferenceEquals(op.OccupiedX, majorana.OccupiedX));
+        Assert.False(ReferenceEquals(op.OccupiedZ, majorana.OccupiedZ));
+    }
+
+    [Fact]
+    public void Negate_MultipliesByMinusOne()
+    {
+        var op = new MajoranaOperator(new BitArray(1), new BitArray(1), Coefficient.PlusI);
+
+        var result = op.Negate();
+
+        var majorana = Assert.IsType<MajoranaOperator>(result);
+        Assert.Equal(Coefficient.MinusI, majorana.Coefficient);
+    }
+
+    [Fact]
+    public void Dual_SwapsOccupiedXAndZ()
+    {
+        var occupiedX = new BitArray(2) { [0] = true };
+        var occupiedZ = new BitArray(2) { [1] = true };
+        var op = new MajoranaOperator(occupiedX, occupiedZ, Coefficient.MinusOne);
+
+        var result = op.Dual();
+
+        var majorana = Assert.IsType<MajoranaOperator>(result);
+        Assert.True(BitsEqual(occupiedZ, majorana.OccupiedX));
+        Assert.True(BitsEqual(occupiedX, majorana.OccupiedZ));
+        Assert.Equal(Coefficient.MinusOne, majorana.Coefficient);
+    }
+
+    [Theory]
+    [MemberData(nameof(IsHermitianCases))]
+    public void IsHermitian_UsesWeightParity(bool[] xBits, bool[] zBits, Coefficient coefficient, bool expected)
+    {
+        var op = new MajoranaOperator(new BitArray(xBits), new BitArray(zBits), coefficient);
+
+        var actual = op.IsHermitian();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Clone_CopiesValues()
+    {
+        var occupiedX = new BitArray(3) { [0] = true };
+        var occupiedZ = new BitArray(3) { [2] = true };
+        var op = new MajoranaOperator(occupiedX, occupiedZ, Coefficient.PlusOne);
+
+        var clone = op.Clone();
+
+        var majorana = Assert.IsType<MajoranaOperator>(clone);
+        Assert.Equal(Coefficient.PlusOne, majorana.Coefficient);
+        Assert.True(BitsEqual(occupiedX, majorana.OccupiedX));
+        Assert.True(BitsEqual(occupiedZ, majorana.OccupiedZ));
+        Assert.False(ReferenceEquals(op.OccupiedX, majorana.OccupiedX));
+        Assert.False(ReferenceEquals(op.OccupiedZ, majorana.OccupiedZ));
+    }
+
+    public static IEnumerable<object[]> IsHermitianCases()
+    {
+        yield return new object[] { new[] { true }, new[] { false }, Coefficient.PlusOne, true };
+        yield return new object[] { new[] { true }, new[] { false }, Coefficient.PlusI, false };
+        yield return new object[] { new[] { true, true }, new[] { false, false }, Coefficient.PlusI, true };
+        yield return new object[] { new[] { true, true }, new[] { false, false }, Coefficient.PlusOne, false };
+    }
+
+    private static bool BitsEqual(BitArray left, BitArray right)
+    {
+        if (left.Length != right.Length) return false;
+
+        for (var i = 0; i < left.Length; i++)
+            if (left[i] != right[i])
+                return false;
+
+        return true;
+    }
+}
---
+++ Lauren.Physics/Operators/MajoranaOperator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Lauren.Physics.Utility;
+
+namespace Lauren.Physics.Operators;
+
+/// <summary>
+///     Represents a Majorana operator in the stabilizer formalism.
+/// </summary>
+/// <param name="occupiedX">γ operators occupied (X basis)</param>
+/// <param name="occupiedZ">γ' operators occupied (Z basis)</param>
+public class MajoranaOperator(BitArray occupiedX, BitArray occupiedZ, Coefficient coefficient = Coefficient.PlusI) :
+    QuantumOperator(occupiedX, occupiedZ, coefficient)
+{
+    public override MajoranaOperator Multiply(QuantumOperator other)
+    {
+        var newOccupiedX = ((BitArray)OccupiedX.Clone()).Xor(other.OccupiedX);
+        var newOccupiedZ = ((BitArray)OccupiedZ.Clone()).Xor(other.OccupiedZ);
+
+        var newCoefficient = Coefficient * other.Coefficient;
+        bool isExchangeParityOdd = ZippedOccupations().ExchangeParityWith(other.ZippedOccupations());
+        if (isExchangeParityOdd)
+            newCoefficient *= Coefficient.MinusOne;
+        return new MajoranaOperator(newOccupiedX, newOccupiedZ, newCoefficient);
+    }
+
+    public override MajoranaOperator Multiply(Coefficient coefficient) =>
+        new((BitArray)OccupiedX.Clone(), (BitArray)OccupiedZ.Clone(), Coefficient * coefficient);
+
+    public override MajoranaOperator Negate() =>
+        new((BitArray)OccupiedX.Clone(), (BitArray)OccupiedZ.Clone(), Coefficient * Coefficient.MinusOne);
+
+    public override MajoranaOperator Dual() =>
+        new((BitArray)OccupiedZ.Clone(), (BitArray)OccupiedX.Clone(), Coefficient);
+
+    public override bool IsHermitian()
+    {
+        int selfWeight = Weight;
+        return selfWeight * (selfWeight - 1) / 2 % 2 == 0 ? Coefficient.IsReal() : Coefficient.IsImaginary();
+    }
+
+    public override MajoranaOperator Clone() =>
+        new((BitArray)OccupiedX.Clone(), (BitArray)OccupiedZ.Clone(), Coefficient);
+}
